e54e6ea149831724e53d5417129d7634ed66c874
soorajjr1951-dev <soorajjr1951@gmail.com>
2026-02-06 17:57:03 +0530

    Initial frontend deployment (Next.js)

--- src/app/(site)/rooms/[slug]/page.js
+++ src/app/(site)/rooms/[slug]/page.js
@@ -8,7 +8,7 @@ import useScrollReveal from "../../../hooks/useScrollReveal";
 import useScrollToTop from "../../../hooks/useScrollToTop";
 import "./room-detail.css";
 
-const WHATSAPP_NUMBER = "+91 8089211075";
+const WHATSAPP_NUMBER = "8089211075";
 
 export default function RoomDetailPage() {
   useScrollToTop();
