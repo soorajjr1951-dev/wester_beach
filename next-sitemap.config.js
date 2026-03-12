77ae6e3e91d39b6d5436bf0c6ffbf229603a6c05
soorajjr1951-dev <soorajjr1951@gmail.com>
2026-03-12 11:33:08 +0530

    generate sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.westernbeachventures.com/",
+  siteUrl: "https://www.westernbeachventures.com",
   generateRobotsTxt: true,
-  sitemapSize: 7000,
 };
